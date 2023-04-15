cea772eaeb4a7498529f382eef5fd1f997da3783
0VitorGabriel <victoropo64@outlook.com>
2023-04-14 20:05:40 -0400

    coloquei as variáveis e o evento de click do input submit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+let first_question = document.getElementsByName('first_question')
+let second_question = document.getElementsByName('second_question')
+let third_question = document.getElementsByName('third_question')
+let fourth_question = document.getElementsByName('fourth_question')
+let fifth_question = document.getElementsByName('fifth_question')
+let sixth_question = document.getElementsByName('sixth_question')
+let seventh_question = document.getElementsByName('seventh_question')
+let eighth_question = document.getElementsByName('eighth_question')
+let ninth_question = document.getElementsByName('ninth_question')
+let tenth_question = document.getElementsByName('tenth_question')
+
+let btn_show_results = document.querySelector('input[type = submit]')
+
+btn_show_results.addEventListener('click', function show_results(event) {
+    event.preventDefault()
+
+
+})
