20e892606fe19db5ac6dbfa8615c6657b85fa251
0VitorGabriel <victoropo64@outlook.com>
2023-04-14 20:13:53 -0400

    adicionei a condição que veririca o botão apertado para mostrar o resultado

--- script/script.js
+++ script/script.js
@@ -1,18 +1,44 @@
-let first_question = document.getElementsByName('first_question')
-let second_question = document.getElementsByName('second_question')
-let third_question = document.getElementsByName('third_question')
-let fourth_question = document.getElementsByName('fourth_question')
-let fifth_question = document.getElementsByName('fifth_question')
-let sixth_question = document.getElementsByName('sixth_question')
-let seventh_question = document.getElementsByName('seventh_question')
-let eighth_question = document.getElementsByName('eighth_question')
-let ninth_question = document.getElementsByName('ninth_question')
-let tenth_question = document.getElementsByName('tenth_question')
+const first_question = document.getElementsByName('first_question')
+const second_question = document.getElementsByName('second_question')
+const third_question = document.getElementsByName('third_question')
+const fourth_question = document.getElementsByName('fourth_question')
+const fifth_question = document.getElementsByName('fifth_question')
+const sixth_question = document.getElementsByName('sixth_question')
+const seventh_question = document.getElementsByName('seventh_question')
+const eighth_question = document.getElementsByName('eighth_question')
+const ninth_question = document.getElementsByName('ninth_question')
+const tenth_question = document.getElementsByName('tenth_question')
 
-let btn_show_results = document.querySelector('input[type = submit]')
+const btn_show_results = document.querySelector('input[type = submit]')
+
+
 
 btn_show_results.addEventListener('click', function show_results(event) {
     event.preventDefault()
 
+    let score = 0
 
+    if (first_question[0].checked) {
+        score++
+    } if (second_question[0].checked) {
+        score++
+    } if (third_question[2].checked) {
+        score++
+    } if (fourth_question[2].checked) {
+        score++
+    } if (fifth_question[0].checked) {
+        score++
+    } if (sixth_question[0].checked) {
+        score++
+    } if (seventh_question[2].checked) {
+        score++
+    } if (eighth_question[3].checked) {
+        score++
+    } if (ninth_question[3].checked) {
+        score++
+    } if (tenth_question[0].checked) {
+        score++
+    }
+
+    alert (score)
 })
